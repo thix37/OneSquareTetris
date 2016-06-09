f53918aade8953a0ac6da760074faf69e4efa419
thix37 <tyncia37@gmail.com>
2016-06-09 20:05:34 +0200

    timer i skret w lewo

--- One-Sq-Tetris/Form1.Designer.cs
+++ One-Sq-Tetris/Form1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+
 namespace One_Sq_Tetris
 {
     partial class Form1
@@ -30,7 +31,6 @@ namespace One_Sq_Tetris
         private void InitializeComponent()
         {
             this.blockPanel = new System.Windows.Forms.Panel();
-            this.blockLabels = new System.Windows.Forms.Label[40];
             this.SuspendLayout();
             // 
             // blockPanel
@@ -40,89 +40,16 @@ namespace One_Sq_Tetris
             this.blockPanel.Size = new System.Drawing.Size(350, 500);
             this.blockPanel.TabIndex = 0;
             this.blockPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-
-            //Tworzymy etykiety
-            int blokNumber = 0;
-            int blokPossitionX = 0;
-            int blokRow = 0;
-
-            foreach (System.Windows.Forms.Label blok in blockLabels)
-            {
-                blockLabels[blokNumber] = new System.Windows.Forms.Label();
-                this.blockLabels[blokNumber].BackColor = Color.Indigo;
-                this.blockLabels[blokNumber].Location = new System.Drawing.Point(60 * blokPossitionX,60 * blokRow);
-                this.blockLabels[blokNumber].Name = "blokLabel" + blokNumber.ToString();
-                this.blockLabels[blokNumber].Size = new System.Drawing.Size(50,50);
-                this.blockLabels[blokNumber].TabIndex = blokNumber;
-                blokNumber++;
-                blokPossitionX++;
-
-
-                if (blokNumber == 5)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-                if (blokNumber == 10)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-                if (blokNumber == 15)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-                if (blokNumber == 20)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-                if (blokNumber == 25)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-                if (blokNumber == 30)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-                if (blokNumber == 35)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-                if (blokNumber == 40)
-                {
-                    blokRow++;
-                    blokPossitionX = 0;
-                }
-
-            }
-
-
-            blokNumber = 0;
-            foreach (System.Windows.Forms.Label blok in blockLabels)
-            {
-                this.blockPanel.Controls.Add(this.blockLabels[blokNumber]);
-                blokNumber++;
-            }
-
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(350, 500);
             this.Controls.Add(this.blockPanel);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tetris One Square";
             this.ResumeLayout(false);
 
         }
@@ -132,6 +59,12 @@ namespace One_Sq_Tetris
         private System.Windows.Forms.Panel blockPanel;
 
         private System.Windows.Forms.Label[] blockLabels;
+
+        public System.Windows.Forms.Label[] BlockLabels
+        {
+            get { return blockLabels; }
+            set { blockLabels = value; }
+        }
 
     }
 }
--- One-Sq-Tetris/Form1.cs
+++ One-Sq-Tetris/Form1.cs
@@ -12,14 +12,97 @@ namespace One_Sq_Tetris
 {
     public partial class Form1 : Form
     {
+        private Color[] blokEmptyColor;
+
+        private Timer _timer;
+
+        private int tetrisPiecePosition;
+
+        private bool[] bloksFilled;
+        
         public Form1()
         {
             InitializeComponent();
+
+            _timer = new Timer();
+            _timer.Interval = 200;
+            _timer.Enabled = true;
+            _timer.Tick+=new System.EventHandler(TimerTickEvent);
+
+            blokEmptyColor = new Color[2];
+
+           bloksFilled = new bool[40];
+
+    
+            tetrisPiecePosition = -3;
+
+            blokEmptyColor[0] = Color.LightCoral;
+            blokEmptyColor[1] = Color.Plum;
+
+            foreach (Label label in BlockLabels)
+            {
+                label.BackColor = blokEmptyColor[0];
+
+            }
+
+            _timer.Start();
+
         }
 
+        private void TimerTickEvent(object sender, EventArgs e)
+        {
+            tetrisPiecePosition += 5;
+
+            if (tetrisPiecePosition > 40)
+            {
+                tetrisPiecePosition = 2;
+
+            }
+
+
+            if (tetrisPiecePosition < 40)
+            {
+                if (BlockLabels[tetrisPiecePosition].BackColor == blokEmptyColor[0])
+                {
+                    BlockLabels[tetrisPiecePosition].BackColor = blokEmptyColor[1];
+
+                    if (tetrisPiecePosition > 5)
+                    {
+                        BlockLabels[tetrisPiecePosition - 5].BackColor = blokEmptyColor[0];
+
+                    }
+                }
+            }
+
+        }
+        
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left)
+            {
+                if (tetrisPiecePosition < 39 && bloksFilled[tetrisPiecePosition]==false)
+                {
+                    if(tetrisPiecePosition!=0 && tetrisPiecePosition!=5 && tetrisPiecePosition!=10 &&
+                        tetrisPiecePosition!=15 && tetrisPiecePosition!=20 && tetrisPiecePosition!=25 &&
+                        tetrisPiecePosition != 30 && tetrisPiecePosition != 35 && BlockLabels[tetrisPiecePosition-1].BackColor!=blokEmptyColor[1])
+                    {
+                        tetrisPiecePosition--;
+                        BlockLabels[tetrisPiecePosition].BackColor = blokEmptyColor[1];
+                        BlockLabels[tetrisPiecePosition + 1].BackColor = blokEmptyColor[0];
+                        return true;
+                    }
+                }
+            }
+            if (keyData == Keys.Right)
+            {
+
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
